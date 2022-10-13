a545c24641673fc61ce5dab2a41ce7d9a7c96ac0
Lennart Schuermann Laptop <schuermann.lennart@web.de>
2022-10-13 16:25:04 +0200

    adding design elements

---
+++ phase_project/lib/design.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+const String kAppName = "Phase Project";
+const String kVersion = "0.0.1";
+
+const kDeleteColor = Color(0xffE74C3C);
+const kRightColor = Color(0xff0be881);
+const kHighlightColor = Color(0xff006EE6);
+
+const kBackgroundColor = Color(0xff101010);
+
+const kTitleSize = 32.0;
+const kHeaderSize = 24.0;
+const kTextSize = 18.0;
+const kDescriptionSize = 10.0;
+
+const kDefaultPadding = 40.0;

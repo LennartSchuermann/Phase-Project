568f68eeaba9ab328066e04fb9c2dfc03dd67bc3
Lennart Schuermann Laptop <schuermann.lennart@web.de>
2022-10-17 16:01:45 +0200

    file handling

--- phase_project/lib/classes/file_handling.dart
+++ phase_project/lib/classes/file_handling.dart
@@ -3,15 +3,49 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
 
-Future<File> saveFile(PlatformFile file) async {
-  final storage = await getApplicationDocumentsDirectory();
-  final dir = Directory('${storage.path}\\PhaseProject\\images');
+String imgPath = 'PhaseProject\\images';
+String dataPath = 'PhaseProject\\save';
 
-  if (await dir.exists()) {
-    dir.create(recursive: true);
-    print(dir.path);
+Future<Directory> getDir(String path) async {
+  final storage = await getApplicationDocumentsDirectory();
+  final dir = Directory('${storage.path}\\$path');
+
+  print(path);
+
+  if (!await dir.exists()) {
+    await dir.create(recursive: true);
   }
+
+  return dir;
+}
+
+//Img Data
+Future<File> saveFile(PlatformFile file) async {
+  final Directory dir = await getDir(imgPath);
 
   final newFile = File('${dir.path}\\${file.name}');
   return File(file.path!).copy(newFile.path);
 }
+
+//Safe data
+
+//Create
+Future<File> createSaveFile() async {
+  final Directory dir = await getDir(dataPath);
+
+  //TODO add template json
+
+  final saveFile = File('${dir.path}\\save.json');
+  return saveFile.create(recursive: true);
+}
+
+//Read
+Future<File> readSaveFile() async {
+  final Directory dir = await getDir(dataPath);
+
+  final saveFile = File('${dir.path}\\save.json');
+  print(saveFile.readAsString());
+  return saveFile;
+}
+
+//Edit
--- phase_project/lib/screens/question_edit_screen.dart
+++ phase_project/lib/screens/question_edit_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';

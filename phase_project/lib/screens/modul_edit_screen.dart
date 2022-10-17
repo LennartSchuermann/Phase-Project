fe84050e1dcd62ef006dfd6e69c718d9df667ce7
Lennart Schuermann Laptop <schuermann.lennart@web.de>
2022-10-17 14:49:51 +0200

    clean up

--- phase_project/lib/screens/modul_edit_screen.dart
+++ phase_project/lib/screens/modul_edit_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:phase_project/design.dart';
 
 import '../prefabs.dart';

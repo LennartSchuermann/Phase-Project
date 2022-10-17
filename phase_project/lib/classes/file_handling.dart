a3955d8fea69890342a4dd26989d33462ec6d0f8
Lennart Schuermann Laptop <schuermann.lennart@web.de>
2022-10-17 15:25:57 +0200

    file picking

--- phase_project/lib/classes/file_handling.dart
+++ phase_project/lib/classes/file_handling.dart
@@ -5,7 +5,13 @@ import 'package:path_provider/path_provider.dart';
 
 Future<File> saveFile(PlatformFile file) async {
   final storage = await getApplicationDocumentsDirectory();
-  final newFile = File('${storage.path}/${file.name}');
+  final dir = Directory('${storage.path}\\PhaseProject\\images');
 
+  if (!await dir.exists()) {
+    dir.create();
+    print(dir.path);
+  }
+
+  final newFile = File('${dir.path}\\${file.name}');
   return File(file.path!).copy(newFile.path);
 }
